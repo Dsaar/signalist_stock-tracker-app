e5dba9bc34df59878e053c5bb9b8ac1faf00af13
Dsaar <danieljoshuasaar@gmail.com>
2026-07-10 14:23:21 +0300

    Implement Search Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,30 +1,27 @@
-'use client'
+'use client';
 
 import {
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuGroup,
     DropdownMenuItem,
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import {LogOut} from "lucide-react";
 import NavItems from "@/components/NavItems";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {useRouter} from "next/navigation";
 import {signOut} from "@/lib/actions/auth.actions";
 
-
-const UserDropdown = ({user}: {user :User }) => {
+const UserDropdown = ({ user, initialStocks }: {user: User, initialStocks: StockWithWatchlistStatus[]}) => {
     const router = useRouter();
 
     const handleSignOut = async () => {
         await signOut();
         router.push("/sign-in");
     }
-
 
     return (
         <DropdownMenu>
@@ -47,7 +44,7 @@ const UserDropdown = ({user}: {user :User }) => {
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+                            <AvatarImage src="https://github.com/shadcn.png" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
                                 {user.name[0]}
                             </AvatarFallback>
@@ -67,9 +64,10 @@ const UserDropdown = ({user}: {user :User }) => {
                 </DropdownMenuItem>
                 <DropdownMenuSeparator className="hidden sm:block bg-gray-600"/>
                 <nav className="sm:hidden">
-                    <NavItems />
+                    <NavItems initialStocks={initialStocks} />
                 </nav>
             </DropdownMenuContent>
-        </DropdownMenu>    )
+        </DropdownMenu>
+    )
 }
 export default UserDropdown

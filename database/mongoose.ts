843e1bb3abf69a357c9a8d020686ed50ac0d2180
Dsaar <danieljoshuasaar@gmail.com>
2026-07-17 08:39:51 +0300

    Implement Watchlist

--- database/mongoose.ts
+++ database/mongoose.ts
@@ -31,7 +31,6 @@ export const connectToDatabase = async () => {
         throw err;
     }
 
-    console.log(`Connected to database ${process.env.NODE_ENV} - ${MONGODB_URI}`);
-
+    console.log("Connected to MongoDB");
     return cached.conn;
 }
